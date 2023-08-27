3f5be129740f2fec45f483da3ac910fe90abdccc
Renan Parise <renan.parise.ifc@gmail.com>
2023-08-26 22:33:14 -0300

    fwefe

--- js/main.js
+++ js/main.js
@@ -650,3 +650,17 @@ var animateReveal = function() {
 
 }
 
+function scrollToTop() {
+	window.scrollTo({
+		top: 0,
+		behavior: "smooth"
+	});
+}
+
+window.onscroll = function() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		document.getElementById("backToTopBtn").style.display = "block";
+	} else {
+		document.getElementById("backToTopBtn").style.display = "none";
+	}
+};
